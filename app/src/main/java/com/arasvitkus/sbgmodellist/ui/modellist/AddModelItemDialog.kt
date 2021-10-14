a31c327b27544a117cb10059eab3c821aa721308
Aras <amvitkus@gmail.com>
2021-10-13 20:56:46 -0400

    Created the dialog_add_model_item.xml for adding models. Created base class of AddModelItemDialog.kt.

---
+++ app/src/main/java/com/arasvitkus/sbgmodellist/ui/modellist/AddModelItemDialog.kt
@@ -0,0 +1,10 @@
+package com.arasvitkus.sbgmodellist.ui.modellist
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatDialog
+
+class AddModelItemDialog(context: Context) : AppCompatDialog(context) {
+
+    //Result of dialog
+    //Send item to Main Activity
+}
